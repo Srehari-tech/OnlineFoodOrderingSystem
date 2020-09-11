1ddefade1331b30492d50800e150d176217080c3
Anand Devarajan <ananddevarajan>
2020-09-11 12:16:07 +0530

    my sql connected

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const port = 3000 || process.env.PORT;
 
+app.use(express.json({ extended: false }));
+app.use(cors());
 app.use('/', (req, res) => {
   res.send('Online Food Ordering System');
 });
