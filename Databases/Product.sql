4cba6de108c663dad34be83965329c1c1f91469c
Anand Devarajan <ananddevarajan>
2020-09-11 12:47:48 +0530

    routes added

---
+++ Databases/Product.sql
@@ -0,0 +1,10 @@
+-- CREATE DATABASE ASSK;
+-- USE ASSK;
+-- CREATE TABLE products (
+--     product_id INT AUTO_INCREMENT,
+--     product_name VARCHAR(30),
+--     price INT,
+--     description VARCHAR(200),
+--     stock INT,
+--     PRIMARY KEY (product_id)
+-- );
